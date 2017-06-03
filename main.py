6ce277d96311e67020c0d67f6ef43423b85d5e93
Miguel Soranno <msoranno@gmail.com>
2017-06-03 19:50:06 +0200

    hemos cambiado de pagina para probar y nos va un poco mejor.

--- main.py
+++ main.py
@@ -3,35 +3,48 @@ import time
 from  bs4 import BeautifulSoup as soup
 
 
-#vcallChannel = "http://www.vestiairecollective.es/novedades/"
-
 v_page=0
 count = 0
-while v_page < 180:
-    v_page += 60
-    print (v_page)
-    vcallChannel = "http://www.vestiairecollective.es/novedades/#_=catalog&limit="+str(v_page)+"&priceMin=0&priceMax=38100&step=60"
-    print (vcallChannel)
+while v_page < 500:
+    v_page += 1
+    #print (v_page)
+    urlpage = "https://www.chicfy.com/mas-nuevo/"+ str(v_page)
+    #print (urlpage)
     #Abrimos la conexion y nos traemos la pagina
-    uCliente = request.urlopen(vcallChannel)
+    uCliente = request.urlopen(urlpage)
 
     #metemos el contenido de la pagina en una variable
-    time.sleep(20)
-    html_vcallChannel = uCliente.read()
+    #time.sleep(20)
+    html_urlpage = uCliente.read()
 
     #cerramos la conexion
     uCliente.close()
 
     #parseamos el html con soup
-    soup_vcallChannel = soup(html_vcallChannel, "html.parser")
-
+    soup_urlpage = soup(html_urlpage, "html.parser")
     #buscamos los divs que necesitamos (antes hemos inspeccionado los divs)
-    containers = soup_vcallChannel.find_all("div", {"class" : "col-xs-6 col-sm-4"})
+    gradas =  soup_urlpage.find_all("h4", {"class" : "grada"})
+    containersL = soup_urlpage.find_all("div", {"class" : "left"})
+    containersR = soup_urlpage.find_all("div", {"class": "right"})
+    modelos = soup_urlpage.find_all("div", {"id": "social-links2"})
 
-    for container in containers:
+    #print (len(containers))
+    # containersRight = soup_urlpage.find_all("div", {"class": "right"})
+    # print (containersLeft[0].a.text)
+    # print(containersLeft[0].p.text)
+    # print (containersRight[0].div())
+    #
+    # print ("--")
+    # print (containersLeft[0])
+    #print(containersR[0])
+    for cL, cR, grada, modelo in zip(containersL,containersR, gradas, modelos) :
         count += 1
-        marca = container.a["data-brand"]
-        pid   = container.a["data-id"]
-        precio = container.a["data-price"]
-        titulo = container.a["data-title"]
-        print (count, ")", marca, ",", pid , ",",  precio, ",",  titulo )
+        marca = grada.text
+        producto = cL.a.text
+        texto = cL.p.text
+        precioactual = cR.div.text
+        precioanterior = cR.span.text
+        usuario = modelo.h5.a.text
+
+        if usuario.strip() == "Estrellita34":
+         print (count , ",", usuario, ",",  marca, "'", producto, ",", texto , ",", precioactual.strip(), ",", precioanterior.strip() )
