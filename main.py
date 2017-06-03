5986eb3f3a0dbffe2ea959f22a5eb67b43ab427b
Miguel Soranno <msoranno@gmail.com>
2017-06-03 23:39:25 +0200

    intentando meter todo en fichero.

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ from  bs4 import BeautifulSoup as soup
 
 v_page=0
 count = 0
-while v_page < 500:
+file = open("chick.csv","w")
+file.write("contador" + ","+ "usuario" + "," + "marca" + "," + "producto" + "," + "texto" + ","  + "precioActual" + "," + "precioAnterior")
+while v_page < 3000:
     v_page += 1
-    #print (v_page)
-    urlpage = "https://www.chicfy.com/mas-nuevo/"+ str(v_page)
+    print ("buscando en pagina:" , v_page)
+    #urlpage = "https://www.chicfy.com/mas-nuevo/"+ str(v_page)
+    #urlpage = "https://www.chicfy.com/user/antonialop/" + str(v_page)
+    urlpage = "https://www.chicfy.com/tienda/" + str(v_page)
     #print (urlpage)
     #Abrimos la conexion y nos traemos la pagina
     uCliente = request.urlopen(urlpage)
@@ -21,30 +25,27 @@ while v_page < 500:
     uCliente.close()
 
     #parseamos el html con soup
-    soup_urlpage = soup(html_urlpage, "html.parser")
+    soup_urlpage = soup(html_urlpage, "html.parser", )
     #buscamos los divs que necesitamos (antes hemos inspeccionado los divs)
     gradas =  soup_urlpage.find_all("h4", {"class" : "grada"})
     containersL = soup_urlpage.find_all("div", {"class" : "left"})
     containersR = soup_urlpage.find_all("div", {"class": "right"})
     modelos = soup_urlpage.find_all("div", {"id": "social-links2"})
 
-    #print (len(containers))
-    # containersRight = soup_urlpage.find_all("div", {"class": "right"})
-    # print (containersLeft[0].a.text)
-    # print(containersLeft[0].p.text)
-    # print (containersRight[0].div())
-    #
-    # print ("--")
-    # print (containersLeft[0])
-    #print(containersR[0])
     for cL, cR, grada, modelo in zip(containersL,containersR, gradas, modelos) :
         count += 1
-        marca = grada.text
-        producto = cL.a.text
-        texto = cL.p.text
-        precioactual = cR.div.text
-        precioanterior = cR.span.text
-        usuario = modelo.h5.a.text
+        marca = grada.text.encode("utf-8")
+        producto = cL.a.text.encode("utf-8")
+        texto = cL.p.text.encode("utf-8")
+        precioactual = cR.div.text.encode("utf-8")
+        precioanterior = cR.span.text.encode("utf-8")
+        usuario = modelo.h5.a.text.encode("utf-8")
 
-        if usuario.strip() == "Estrellita34":
-         print (count , ",", usuario, ",",  marca, "'", producto, ",", texto , ",", precioactual.strip(), ",", precioanterior.strip() )
+        file.write(str(count) + "," + str(usuario) + "," + str(marca) + "," + str(producto) + "," + str(texto) + ","  + str(precioactual.strip()) + "," + str(precioanterior.strip()))
+        print(count, ",", usuario, ",", marca, "'", producto, ",", texto, ",", precioactual.strip(), ",",precioanterior.strip())
+        # if "maison" in usuario.strip():
+        #   print ("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+        #   print (count , ",", usuario, ",",  marca, "'", producto, ",", texto , ",", precioactual.strip(), ",", precioanterior.strip() )
+        #   print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+
+file.close()
